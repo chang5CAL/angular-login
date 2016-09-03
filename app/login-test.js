6c083202c9b45d73dad4bd9ea01934b528e6fb5b
chang5CAL <chang5@calcharter.org>
2016-09-02 21:15:16 -0700

    adjusted tests

---
+++ app/login-test.js
@@ -0,0 +1,19 @@
+"use strict";
+describe('LoginServices', function () {
+    it('JasmineTest', function () {
+        expect(1).toEqual(1);
+    });
+    /*
+        it('validUser', () => {
+            let logged = LoginService.={};
+    
+            expect(logged).toEqual(true);
+        });
+    
+        it('invalidUser', () => {
+            let logged: login={email: 'string', password: 'string'};
+    
+            expect(logged).toEqual(false);
+        });*/
+});
+//# sourceMappingURL=login-test.js.map
---
+++ app/login-test.ts
@@ -0,0 +1,20 @@
+import { LoginService } from './login.service'
+
+describe('LoginServices', () => {
+
+	it('JasmineTest', () => {
+		expect(1).toEqual(1);
+	})
+/*
+	it('validUser', () => {
+		let logged = LoginService.={};
+
+		expect(logged).toEqual(true);
+	});
+
+	it('invalidUser', () => {
+		let logged: login={email: 'string', password: 'string'};
+
+		expect(logged).toEqual(false);
+	});*/
+});
